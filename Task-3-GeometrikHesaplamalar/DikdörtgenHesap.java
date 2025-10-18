fadf63b875bb0b389cebb1b8a7b115895e6a3a87
Ali-Istanbullu <250542019@firat.edu.tr>
2025-10-18 21:46:38 +0300

    DikdörtgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdörtgenHesap.java
@@ -0,0 +1,13 @@
+public class DikdörtgenHesap {
+
+     public static void main(String[] args) {
+        double En = 4.5;
+        double Boy = 7.9;
+        double Alan = En*Boy;
+        double Cevre = 2*En+2*Boy;
+
+        System.out.printf("Alan = %.3f\n",Alan);
+        System.out.println("Çevre = "+Cevre);
+
+     }
+}
