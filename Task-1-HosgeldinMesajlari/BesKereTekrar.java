5c01e83c0c104963ad03a1b11af28abaec7b3b9b
Ali-Istanbullu <250542019@firat.edu.tr>
2025-10-18 17:34:17 +0300

    Update BesKereTekrar.java
    ödev1 5tekrar = TAMAMLANDI

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,15 @@
+public class deneme1 {
+
+public static void main(String[] args) {
+
+ System.out.println("Java'ya Hoş Geldiniz");
+ System.out.println("Java'ya Hoş Geldiniz");
+ System.out.println("Java'ya Hoş Geldiniz");
+ System.out.println("Java'ya Hoş Geldiniz");
+ System.out.println("Java'ya Hoş Geldiniz");
+ 
+}
+
+
+
+}
