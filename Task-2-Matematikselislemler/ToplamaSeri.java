fb83ddd868f850d02e9cda371a625c2a4d627075
Ali-Istanbullu <250542019@firat.edu.tr>
2025-10-18 18:34:08 +0300

    Update ToplamaSeri.java
    ödev2 toplama = TAMAMLANDI

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,19 @@
+public class deneme1 {
+
+public static void main(String[] args) {
+
+ int sayilarin_toplami =1+2+3+4+5+6+7+8+9;
+
+
+
+ System.out.println("1+2+3+4+5+6+7+8+9 = "+sayilarin_toplami  );
+ 
+ 
+
+
+
+}
+
+
+
+}
