f6805c08fac4990149b572b0f1f5ec327d9deba4
Ali-Istanbullu <250542019@firat.edu.tr>
2025-10-18 18:59:05 +0300

    Update IkiIslem.java
    ödev2 2işlem = TAMAMLANDI

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,21 @@
+public class deneme1 {
+
+public static void main(String[] args) {
+
+ double pay =9.5*4.5-2.5*3;
+ double payda =45.5-3.5;
+ double işlem =pay/payda;
+
+
+
+ System.out.printf("Sonuç= %.4f/n",işlem );
+ 
+ 
+
+
+
+}
+
+
+
+}
