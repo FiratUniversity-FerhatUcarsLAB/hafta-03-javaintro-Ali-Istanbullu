75496ada30c759a06c8486582e631ff389ee76da
Ali-Istanbullu <250542019@firat.edu.tr>
2025-10-20 16:18:21 +0300

    UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,25 @@
+public class UsluSayilarTablosu{
+
+ public static void main(String[] args){
+
+   int A1 = 1;
+   int A2 = 2;
+   int A3 = 3;
+   int A4 = 4;
+   int A5 = 5;
+   
+
+
+   System.out.println("a^1   a^2   a^3");
+   System.out.println("---------------");
+   System.out.println( " "+A1+"     "+A1+"     "+A1);
+   System.out.println( " "+(A2)+"     "+(A2*A2)+"     "+(A2*A2*A2));
+   System.out.println( " "+(A3)+"     "+(A3*A3)+"     "+(A3*A3*A3));
+   System.out.println( " "+(A4)+"     "+(A4*A4)+"    "+(A4*A4*A4));
+   System.out.println( " "+(A5)+"     "+(A5*A5)+"    "+(A5*A5*A5));
+   
+
+ }
+
+
+}
