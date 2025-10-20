af5d049198c4a6fa783a4153cb9ea910208e7f27
Ali-Istanbullu <250542019@firat.edu.tr>
2025-10-20 13:53:08 +0300

    IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,25 @@
+public class IsimKarti{
+
+    public static void main (String[] args){
+
+    String ad ="ali";
+    String soyad ="İstanbullu";
+    String ogrno ="250542019";
+    String bolum ="yazilim muh";
+
+
+     System.out.println("----------------------");
+     System.out.println("OGRENCI BILGILERI");
+     System.out.println("----------------------");
+     System.out.println("Ad = "+ad);
+     System.out.println("Soyad = "+soyad);
+     System.out.println("Ogrenci no = "+ogrno);
+     System.out.println("Bolum = "+bolum);
+      System.out.println("----------------------");
+
+
+
+    }
+
+
+}
