b687d165fb5fb1949aad67e4d333084ab6d12726
Ali-Istanbullu <250542019@firat.edu.tr>
2025-10-20 18:00:45 +0300

    MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,25 @@
+public class MilKilometreTablosu{
+    public static void main(String[] args){
+
+      double mil = 1.609;
+      double birmil = mil*1;
+      double besmil = mil*5;
+      double onmil = mil*10;
+      double yirmimil = mil*20;
+      double ellimil = mil*50;
+      System.out.println("-----|-------|--------|--------|---------|");
+      System.out.println("1mil |  5mil |  10mil |  20mil |  50mil  |");
+      System.out.println("-----|-------|--------|--------|---------|");
+      System.out.println(birmil+"| "+besmil+" | "+onmil+"  | "+yirmimil+"  | "+ellimil+"   | (km)");
+      System.out.println("-----|-------|--------|--------|---------|");
+
+
+
+
+
+
+
+
+
+    }
+}
