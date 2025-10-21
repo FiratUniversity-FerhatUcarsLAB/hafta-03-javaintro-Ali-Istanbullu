71f603a717cd47e6170184e5d53f44987716ff3f
Ali-Istanbullu <250542019@firat.edu.tr>
2025-10-21 10:55:02 +0300

    CarpimTablosu.java

---
+++ Task-5-TabloDesenleri/CarpimTablosu.java
@@ -0,0 +1,20 @@
+public class CarpimTablosu{
+    public static void main(String[] args){
+
+
+     int a = 2 ;
+     int b = 1 ;
+     String f ="--------------------";
+
+     for (b=1; b<=10; b++) {
+     
+     int c = a*b;
+     
+     System.out.println(a+"x"+b+" = "+c);
+     System.out.println(f); 
+
+     }
+     
+     
+    }
+}
